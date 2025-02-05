4da73aad1b26566d76928feab85ff914cf4c6865
shivamjaryal <143475213+shivamjaryal@users.noreply.github.com>
2025-02-05 12:04:26 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	target/demo-0.0.1-SNAPSHOT.jar
    #	target/demo-0.0.1-SNAPSHOT.jar.original
    #	target/surefire-reports/TEST-com.example.demo.DemoApplicationTests.xml
    #	target/surefire-reports/com.example.demo.DemoApplicationTests.txt

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController//rest
+@RestController//rest3
 @RequestMapping("/emp")
 public class EmployeeController {
 
